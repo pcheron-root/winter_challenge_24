f936464a19f4e5d95c6758183ce133d874d63ba3
Mathieu Soysal <hsoysal@student.42.fr>
2024-12-11 11:13:10 +0000

    Update benchmark to use codingame_rust_template for Fibonacci function

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,15 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use template_exercisme::fib::fibonacci;
+use codingame_rust_template::fib::fibonacci;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
-    c.bench_function("fib 30", |b| b.iter(|| fibonacci(black_box(30))));
-    c.bench_function("fib 40", |b| b.iter(|| fibonacci(black_box(40))));
-    c.bench_function("fib 50", |b| b.iter(|| fibonacci(black_box(50))));
-    c.bench_function("fib 60", |b| b.iter(|| fibonacci(black_box(60))));
-    c.bench_function("fib 70", |b| b.iter(|| fibonacci(black_box(70))));
-    c.bench_function("fib 80", |b| b.iter(|| fibonacci(black_box(80))));
-    c.bench_function("fib 90", |b| b.iter(|| fibonacci(black_box(90))));
+    c.bench_function("fib 20", |b| b.iter(|| black_box(fibonacci(20))));
+    c.bench_function("fib 30", |b| b.iter(|| black_box(fibonacci(30))));
+    c.bench_function("fib 40", |b| b.iter(|| black_box(fibonacci(40))));
+    c.bench_function("fib 50", |b| b.iter(|| black_box(fibonacci(50))));
+    c.bench_function("fib 60", |b| b.iter(|| black_box(fibonacci(60))));
+    c.bench_function("fib 70", |b| b.iter(|| black_box(fibonacci(70))));
+    c.bench_function("fib 80", |b| b.iter(|| black_box(fibonacci(80))));
+    c.bench_function("fib 90", |b| b.iter(|| black_box(fibonacci(90))));
 }
 
 criterion_group!(benches, criterion_benchmark);
