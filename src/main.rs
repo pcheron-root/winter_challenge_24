59bcde9e86ece597d0a0e42d2ed3549941b440c2
Mathieu Soysal <hsoysal@student.42.fr>
2024-12-11 12:49:20 +0000

    feat: Add Fibonacci module and update main function to compute Fibonacci of input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+pub mod fib;
+use fib::fibonacci;
+
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let _n = args
+    let n = args
         .get(1)
         .expect("Give one argument")
         .parse::<u32>()
         .expect("Given argument should be a integer.");
+
+    println!("{}", fibonacci(n));
 }
